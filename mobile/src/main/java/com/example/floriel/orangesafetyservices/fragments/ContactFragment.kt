bed6f8a1b37b208d7e59af56b4ca2fafef884164
Floriel Fedry <Ffloriel@users.noreply.github.com>
2016-12-08 13:43:59 +0100

    Add on item swipe delete for contact list element

--- mobile/src/main/java/com/example/floriel/orangesafetyservices/fragments/ContactFragment.kt
+++ mobile/src/main/java/com/example/floriel/orangesafetyservices/fragments/ContactFragment.kt
@@ -3,6 +3,7 @@ package com.example.floriel.orangesafetyservices.fragments
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +12,9 @@ import com.example.floriel.orangesafetyservices.R
 import com.example.floriel.orangesafetyservices.objects.Contact
 import com.example.floriel.orangesafetyservices.objects.ContactDao
 import com.example.floriel.orangesafetyservices.recyclers.ContactRAdapter
+import com.example.floriel.orangesafetyservices.recyclers.helper.SimpleItemTouchHelperCallback
 
-class ContactFragment : BaseFragment() {
+class ContactFragment : BaseFragment(), OnStartDragListener {
 
     private lateinit var mRecyclerViewEmergency: RecyclerView
     private lateinit var mRecyclerViewSafety: RecyclerView
@@ -20,6 +22,8 @@ class ContactFragment : BaseFragment() {
     private lateinit var mContactsSafety: MutableList<Contact>
     private lateinit var mAdapterEmergency: ContactRAdapter
     private lateinit var mAdapterSafety: ContactRAdapter
+    private lateinit var mItemTouchHelper: ItemTouchHelper
+    private lateinit var mContactDao: ContactDao
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_contact, container, false)
@@ -27,29 +31,37 @@ class ContactFragment : BaseFragment() {
         mRecyclerViewEmergency = view.findViewById(R.id.emergencyList) as RecyclerView
         mRecyclerViewSafety = view.findViewById(R.id.safetyList) as RecyclerView
 
-        mRecyclerViewEmergency.layoutManager = LinearLayoutManager(this.activity)
-        mAdapterEmergency = ContactRAdapter(mContactsEmergency)
-        mRecyclerViewEmergency.adapter = mAdapterEmergency
-
-        mRecyclerViewSafety.layoutManager = LinearLayoutManager(this.activity)
-        mAdapterSafety = ContactRAdapter(mContactsSafety)
-        mRecyclerViewSafety.adapter = mAdapterSafety
-
         return view
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onStart() {
+        super.onStart()
         val daoSession = (this.activity.application as App).getDaoSession()
-        val contactDao = daoSession.contactDao
-        mContactsEmergency = contactDao.queryBuilder()
+        mContactDao = daoSession.contactDao
+        mContactsEmergency = mContactDao.queryBuilder()
                 .where(ContactDao.Properties.Type.eq(2))
                 .orderAsc(ContactDao.Properties.Name)
                 .list()
-        mContactsSafety = contactDao.queryBuilder()
+        mContactsSafety = mContactDao.queryBuilder()
                 .where(ContactDao.Properties.Type.eq(1))
                 .orderAsc(ContactDao.Properties.Name)
                 .list()
+
+        mRecyclerViewEmergency.layoutManager = LinearLayoutManager(this.activity)
+        mAdapterEmergency = ContactRAdapter(mContactsEmergency, mContactDao)
+        mRecyclerViewEmergency.adapter = mAdapterEmergency
+
+        mRecyclerViewSafety.layoutManager = LinearLayoutManager(this.activity)
+        mAdapterSafety = ContactRAdapter(mContactsSafety, mContactDao)
+        mRecyclerViewSafety.adapter = mAdapterSafety
+
+        val callback = SimpleItemTouchHelperCallback(mAdapterSafety)
+        mItemTouchHelper = ItemTouchHelper(callback)
+        mItemTouchHelper.attachToRecyclerView(mRecyclerViewSafety)
+    }
+
+    override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
+
     }
 
     companion object {
@@ -63,6 +75,10 @@ class ContactFragment : BaseFragment() {
         }
     }
 
+}
+
+interface OnStartDragListener {
+    fun onStartDrag(viewHolder: RecyclerView.ViewHolder)
 }
 
 
