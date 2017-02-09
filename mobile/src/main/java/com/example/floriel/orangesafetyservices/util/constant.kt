4664086a14b84414aa651ddb7631594e0a83401a
Floriel Fedry <Ffloriel@users.noreply.github.com>
2017-02-09 18:49:39 +0100

    Delete non used files

--- mobile/src/main/java/com/example/floriel/orangesafetyservices/util/constant.kt
+++ mobile/src/main/java/com/example/floriel/orangesafetyservices/util/constant.kt
@@ -3,6 +3,7 @@ package com.example.floriel.orangesafetyservices.util
 class Constant {
 
     companion object {
+        val KEY_CONTACT_TYPE = "contact type key"
         val EMERGENCY_CONTACT_SELECTION = 1
         val SAFETY_CONTACT_SELECTION = 2
     }
